04f05aae59dde7b3722f0403ab14e7f909b13651
AlexanderMann <mannimal09@gmail.com>
2019-03-17 12:03:00 -0500

    Feature: Align all svg srcs to the center of the first

--- src/patterns/utils/layout/align.clj
+++ src/patterns/utils/layout/align.clj
@@ -1,1 +1,32 @@
-(ns patterns.utils.layout.align)
+(ns patterns.utils.layout.align
+  (:require [patterns.utils :as utils]
+            [patterns.utils.svg :as svg]))
+
+(defn center
+  [[src & srcs]]
+  (let [{w :width
+         h :height
+         :as dims} (svg/dimensions src)
+        gen-id (partial gensym "center")
+        id (gen-id)
+        ids (repeatedly (count srcs) gen-id)]
+    (utils/veccat
+      [:svg dims
+       (utils/veccat
+         [:defs {}
+          (svg/->def src id)]
+         (map svg/->def
+              srcs
+              ids))
+       (svg/use id {})]
+      (map (fn [src id]
+             (let [{:keys [width height]} (svg/dimensions src)
+                   gap-width (float (/ (- w width)
+                                       2))
+                   gap-height (float (/ (- h height)
+                                        2))]
+               (svg/use id
+                        {:x gap-width
+                         :y gap-height})))
+           srcs
+           ids))))
