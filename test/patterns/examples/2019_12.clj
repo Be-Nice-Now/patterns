7e67becd7b0e0853d9dea57dcecd426db773dc06
AlexanderMann <mannimal09@gmail.com>
2019-04-03 09:11:38 -0500

    2019-12: Basic triangles

--- test/patterns/examples/2019_12.clj
+++ test/patterns/examples/2019_12.clj
@@ -14,8 +14,14 @@
   "Given a colour, determine the ideal graytone for it. ie, shift it so that it resides
   more in the gray colour space."
   [{:keys [a r g b]}]
-  (let [diff (- ideal-gray
-                (max r g b))]
+  (let [max-rgb (max r g b)
+        diff (if (> ideal-gray
+                    max-rgb)
+               (- ideal-gray
+                  (max r g b))
+               (int (/ (- 255
+                          max-rgb)
+                       2)))]
     {:a a
      :r (+ r diff)
      :g (+ g diff)
@@ -30,75 +36,82 @@
 
 (defn triangle-swatch
   [n primary-colour secondary-colour]
-  (let [stroke-width 10
-        mask-id (gensym "m")
-        triangles (->> (range 0 101 (int (/ 100 (* 2 n))))
-                       rest
-                       (map (fn [filled? dim]
-                              [:svg {:width dim
-                                     :height dim}
-                               [:defs {}]
-                               (polygon/equilateral
-                                 3
-                                 (- (float (/ dim
-                                              2))
-                                    stroke-width)
-                                 {:style (format "fill:%s;"
-                                                 (if filled?
-                                                   "black"
-                                                   "white")
-                                                 stroke-width)})])
-                            (cycle [true false]))
-                       reverse
-                       align/center)]
-    [:svg {:width 100
-           :height 100}
-     [:defs {}
-      [:mask {:id mask-id} triangles]]
-     [:rect {:width 100
-             :height 100
-             :x 0 :y 0
-             :fill (color/map-> (->graytone primary-colour))
-             :mask (format "url(#%s)"
-                           mask-id)}]]))
+  (let [stroke-width 2]
+    (->> (range 100 stroke-width (- (/ (- 100
+                                          stroke-width)
+                                       n)))
+         (map #(-> %
+                   float
+                   Math/round
+                   int))
+         (map (fn [dim]
+                [:svg {:width (* 2 (+ dim stroke-width))
+                       :height (* 2 (+ dim stroke-width))}
+                 [:defs {}]
+                 (polygon/equilateral
+                   3
+                   (- dim
+                      stroke-width)
+                   {:style (format "fill:none;stroke:%s;stroke-width:%s"
+                                   (color/map-> (->graytone secondary-colour))
+                                   stroke-width)})]))
+         align/center)))
 
-#_(defn needle-swatch
-    [a b colour]
-    (let [TODO--radius--percentage 0.1
-          circle-radius (* TODO--radius--percentage
-                           (line-length a b))]
-      [:path {}]))
-
-(defn needles--b-coord
-  [a angle radius]
-  :TODO)
-
-(defn needle-coord
-  [{:keys [width height]} padding]
-  (let [TODO--radius 200
-        coord-gen (fn [dimension]
-                    (+ padding
-                       (rand-int (- dimension (* 2
-                                                 padding)))))
-        a {:x (coord-gen width)
-           :y (coord-gen height)}]
-    {:a a
-     :b (needles--b-coord a
-                          (rand-int 360)
-                          TODO--radius)}))
-
-(defn needle-coords
-  [dims padding n]
-  (repeatedly n
-              (partial needle-coord
-                       dims
-                       padding)))
+(defn- triangle-transform
+  [{src-width :width
+    src-height :height} {:keys [width height]} id]
+  (let [scale (float (inc (rand)))
+        x (int (+ (rand-int (- width
+                               (* 2 src-width scale)))
+                  (* src-width scale)))
+        y (int (+ (rand-int (- height
+                               (* 2 src-height scale)))
+                  (* src-height scale)))]
+    (svg/use id {:x x
+                 :y y
+                 :transform (format "rotate(%s %s %s) scale(%s)"
+                                    (rand-int 360)
+                                    (int (+ x (/ src-width
+                                                 2)))
+                                    (int (+ y (/ src-height
+                                                 2)))
+                                    scale)})))
 
 (defn gen
   []
-  (let [gen-fn identity]
-    (doseq [[idx_1_based year month day] (e/indexed-days-of-week (e/week->date 2019 12))]
-      (gen-fn idx_1_based year month day))))
+  (let [gen-fn (fn [idx_1_based year month day [primary-colour secondary-colour]]
+                 (let [triangle-id (gensym "t")
+                       triangle-swatch (triangle-swatch day
+                                                        primary-colour
+                                                        secondary-colour)]
+                   (e/render [year month day]
+                             (utils/veccat
+                               [:svg {:width e/INSTAGRAM-RECOMMENDED-MIN-WIDTH-HEIGHT
+                                      :height e/INSTAGRAM-RECOMMENDED-MIN-WIDTH-HEIGHT}
+                                [:defs {}
+                                 (svg/->def triangle-swatch
+                                            triangle-id)]
+                                (background-swatch (->graytone primary-colour))]
+                               (repeatedly (* month day)
+                                           #(triangle-transform
+                                              (svg/dimensions triangle-swatch)
+                                              {:width e/INSTAGRAM-RECOMMENDED-MIN-WIDTH-HEIGHT
+                                               :height e/INSTAGRAM-RECOMMENDED-MIN-WIDTH-HEIGHT}
+                                              triangle-id)))
+                             ""
+                             {:recursive? false})))
+        colours (->> e/poke-palettes
+                     (shuffle)
+                     (take 7)
+                     (map (comp (partial take 4) rest))
+                     (map (fn [xs]
+                            (map second
+                                 xs))))]
+    (doseq [[[idx_1_based year month day] colours]
+            (map list
+                 (e/indexed-days-of-week (e/week->date 2019 12))
+                 colours)]
+      (gen-fn idx_1_based year month day colours))))
 
 (comment
   (trace/profile {} (gen)))
